f05f6713799bc97fc23bed201ae211deafde4bd3
Gerardo Cardona <chardon6@hotmail.com>
2022-07-27 22:08:10 -0300

    chng login y skills

---
+++ js/skills.js
@@ -0,0 +1,8 @@
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},3000);
+	});
+}); 
